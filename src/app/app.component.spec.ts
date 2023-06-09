af92fa93248e548b1e502a0fe4c8656372e1c440
Charles Leach <cleach@cs6261-vm02.uwg.westga.edu>
2023-06-09 14:06:07 -0400

    afterchanpter5withsomeerrors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,76 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { Stock } from './model/stock';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  describe('Simple, No Angular Unit Test', () => {
+    it('should have stock instantiated on ngInit', () => {
+      const appComponent = new AppComponent();
+      expect(appComponent.stock).toBeUndefined();
+      appComponent.ngOnInit();
+      expect(appComponent.stock).toEqual(
+        new Stock('Test Stock Company', 'TSC', 85, 80));
+    });
+
+    it('should have toggle stock favorite', () => {
+      const appComponent = new AppComponent();
+      appComponent.ngOnInit();
+      expect(appComponent.stock.favorite).toBeFalsy();
+      appComponent.onToggleFavorite(new Stock('Test', 'TEST', 54, 55));
+      expect(appComponent.stock.favorite).toBeTruthy();
+      appComponent.onToggleFavorite(new Stock('Test', 'TEST', 54, 55));
+      expect(appComponent.stock.favorite).toBeFalsy();
+    });
   });
 
-  it(`should have as title 'stock-market'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('stock-market');
+  describe('Angular-Aware test', () => {
+
+    let fixture, component;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          StockItemComponent,
+        ],
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should load stock with default values', () => {
+      const titleEl = fixture.debugElement.query(By.css('h1'));
+      // Trim to avoid HTML whitespaces
+      expect(titleEl.nativeElement.textContent.trim()).toEqual('Stock Market App');
+
+      // Check for default stock values in template
+      const nameEl = fixture.debugElement.query(By.css('.name'));
+      expect(nameEl.nativeElement.textContent).toEqual('Test Stock Company (TSC)');
+      const priceEl = fixture.debugElement.query(By.css('.price.positive'));
+      expect(priceEl.nativeElement.textContent).toEqual('$ 85');
+      const addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeDefined();
+    });
+
+    it('should toggle stock favorite correctly', () => {
+      expect(component.stock.favorite).toBeFalsy();
+      let addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeDefined();
+      addToFavoriteBtnEl.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+      expect(component.stock.favorite).toBeTruthy();
+      addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeNull();
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('stock-market app is running!');
-  });
 });
